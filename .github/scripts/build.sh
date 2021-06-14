17dbfb7d2a93be2d3a6762b0c4c443df92abda89
plehegar <plh@w3.org>
2021-06-14 15:59:23 -0400

    more ls

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,16 @@
-echo This is a script
-echo The directory is $PATH
-echo The directory is $PWD
-echo GitHub version
+#!/bin/sh
+
+echo -- This is a script
+echo -- The directory is $PATH
+echo ----
+echo -- GitHub version
 git --version
+
+echo "Directory Content $PWD"
+ls -la
+
+cd foobar
+
+echo "Directory Content $PWD"
 ls -la
 
