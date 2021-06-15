b5aea2b1c61e1e3cac9b36775c32cfc08c911f3a
plehegar <plh@w3.org>
2021-06-15 12:06:08 -0400

    Deploy test

---
+++ .github/scripts/deploy-output.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ev
+STATUS=`git log -1 --pretty=oneline`
+
+rm -rf out
+mkdir out
+cp index.html ./out/
+
+cd out
+git init
+git config user.name "GitHub-Action"
+git config user.email "github-action"
+ls
+git add .
+git commit -m "Built by GitHub action: ${STATUS}"
+git status
+
+// GITHUB_TOKEN
+//
+
+FULL_REPO=`https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git/`
+git push --force --quiet $FULL_REPO master:gh-pages
