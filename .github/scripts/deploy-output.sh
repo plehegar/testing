07a50d17c17360df38ac38c2f31ae8bea3894bd1
plehegar <plh@w3.org>
2021-06-15 12:36:35 -0400

    Use deploy script

--- .github/scripts/deploy-output.sh
+++ .github/scripts/deploy-output.sh
@@ -4,9 +4,32 @@ STATUS=`git log -1 --pretty=oneline`
 
 rm -rf out
 mkdir out
-cp index.html ./out/
 
 cd out
+
+FULL_REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git/"
+
+TARGET_BRANCH="gh-pages"
+
+git remote set-url origin "${FULL_REPO}"
+git pull origin "${TARGET_BRANCH}" --rebase
+
+ls -la
+
+cd ..
+
+ls -la
+
+cp index.html out/testing
+
+cd out/testing
+
+ls -la
+
+git push --force-with-lease origin "${TARGET_BRANCH}"
+
+exit 0
+
 git init
 git config user.name "GitHub-Action"
 git config user.email "github-action@users.noreply.github.com"
@@ -15,6 +38,5 @@ git add .
 git commit -m "Built by GitHub action: ${STATUS}"
 git status
 
-FULL_REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git/"
 
 git push --force --quiet $FULL_REPO master:gh-pages
