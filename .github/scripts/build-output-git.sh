bd5ff29034239484df579602ef0cd95a0e8d3012
plehegar <plh@w3.org>
2021-06-23 10:44:50 -0400

    set -u test

--- .github/scripts/build-output-git.sh
+++ .github/scripts/build-output-git.sh
@@ -1,78 +1,56 @@
 #!/bin/bash
-set -ex
-
-if [ $FOLDER = "" ]
-then
- echo "build-output-git needs a build directory name in FOLDER env variable"
- exit 1
-fi
-
-if [ $GITHUB_TOKEN = "" ]
-then
- echo "build-output-git needs the Github token in GITHUB_TOKEN env variable"
- exit 1
-fi
+set -exu
+# e: Exit immediately if a command exits with a non-zero status
+# x: Print commands and their arguments as they are executed
+# u: Treat unset variables as an error when substituting
 
 git config --global user.email "github-action@users.noreply.github.com"
 git config --global user.name $GITHUB_ACTOR
 git config --global user.password $GITHUB_TOKEN
 
+# Recreate some Travis CI env variables
 TRAVIS_BRANCH=${GH_BRANCH:-$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')}
 TRAVIS_PULL_REQUEST=${GH_EVENT_NUMBER:-false}
 
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
-
-echo "REPO: $REPO"
-echo "SSH_REPO: $SSH_REPO"
-echo "SHA: $SHA"
-echo "GITHUB_SHA: $SHA"
-
-
-
-
-TMPDIR=$FOLDER
-
-echo Cleaning $TMPDIR before building
+echo Cleaning $FOLDER before building
 
 MAIN=$PWD
 
-rm -rf $TMPDIR
-mkdir $TMPDIR
-
-cd $TMPDIR
+rm -rf $FOLDER
 
 REPO_URL="https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 
 echo Using $GITHUB_REPOSITORY
 
-git clone $REPO_URL
+git clone $REPO_URL $FOLDER
 
-cd testing
-
-# git remote set-url origin $REPO_URL
-
-git checkout gh-pages
+cd $FOLDER
 
 TARGET=$PWD
+TARGET_BRANCH=gh-pages
 
-# Place your build operations below
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 
 cd $MAIN
+
+# Place your build operations below
+# new files should go into $TARGET
 
 echo Now building the output in $TARGET
 
 cp index.html ${TARGET}
 cp redirect.html ${TARGET}
 
+# End of building the
+
 cd ${TARGET}
 
 if [ $TRAVIS_PULL_REQUEST != "false" ]
 then
+  # this is a pull request so exit
   exit 0
 fi
 
@@ -87,12 +65,10 @@ git add -A .
 
 echo Commit the changes
 
-git commit -m "Deploy to GitHub Pages: $GITHUB_SHA from branch \"$GITHUB_REF\""
+git commit -m "Deploy to GitHub Pages: $GITHUB_SHA from branch \"$TRAVIS_BRANCH\""
 
 echo Attempt to push
 
-git push --force $REPO_URL gh-pages
+git push $REPO_URL $TARGET_BRANCH
 
 echo done
-
-
